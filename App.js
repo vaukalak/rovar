38c7f3badf19da76a43be04aac052cdf8657e646
vaukalak <lahmataja.pa4vara@gmail.com>
2019-04-14 06:31:59 +0300

    extracted Touchable component

--- App.js
+++ App.js
@@ -1,117 +1,78 @@
-import React, { useState, useEffect } from 'react';
-import { View, Dimensions, Text, Animated as RNAnimated } from 'react-native';
+import React from 'react';
+import { View } from 'react-native';
 import Animated, { Easing } from 'react-native-reanimated';
-import { TapGestureHandler, State } from 'react-native-gesture-handler';
+import colorParser from 'color';
+import Touchable from './lib/Touchable';
 
-const { add, multiply, color, max, min, round, sub, Clock, timing, stopClock, clockRunning, startClock, Value, block, set, event, eq, cond } = Animated;
+const { add, debug, concat, interpolate, multiply, color, round, sub } = Animated;
 
-const opacity = new Animated.Value(0);
-
-const clockIn = new Clock();
-const clockOut = new Clock();
-// const clockOut = clockIn;
-
-const animationState = {
-  finished: new Value(0),
-  position: new Value(0),
-  time: new Value(0),
-  frameTime: new Value(0),
+const interpolateColor = (anim, from, to) => {
+  const colorFrom = colorParser(from).object();
+  const colorTo = colorParser(to).object();
+  return color(
+    round(interpolate(anim, {
+      inputRange: [0, 1],
+      outputRange: [colorFrom.r, colorTo.r],
+    })),
+    round(interpolate(anim, {
+      inputRange: [0, 1],
+      outputRange: [colorFrom.g, colorTo.g],
+    })),
+    round(interpolate(anim, {
+      inputRange: [0, 1],
+      outputRange: [colorFrom.b, colorTo.b],
+    })),
+  );
 };
 
-const config = {
-  duration: 300,
-  toValue: new Value(0),
-  easing: Easing.in(Easing.quad),
-  // easing: Easing.ease,
-};
-
-const handler = event([
-  {
-    nativeEvent: ({ state }) => {
-      return block([
-        cond(eq(state, State.BEGAN), [
-          cond(clockRunning(clockIn), [
-            stopClock(clockOut),
-            set(config.toValue, 1),
-          ], [
-            set(animationState.finished, 0),
-            set(animationState.time, 0),
-            set(animationState.frameTime, 0),
-            set(config.toValue, 1),
-            startClock(clockIn),
-          ]),
-          timing(clockIn, animationState, config),
-          cond(state.finished, stopClock(clockIn)),
-
-          set(opacity, animationState.position),
-        ], [
-          cond(
-            eq(state, State.END),
-            [
-              stopClock(clockIn),
-
-              cond(clockRunning(clockOut), [
-                set(config.toValue, 0),
-              ], [
-                set(animationState.finished, 0),
-                set(animationState.time, 0),
-                set(animationState.frameTime, 0),
-                set(config.toValue, 0),
-                startClock(clockOut),
-              ]),
-              timing(clockOut, animationState, config),
-              cond(state.finished, stopClock(clockOut)),
-
-              set(opacity, animationState.position),
-            ]
-          ),
-        ]),
-      ]);
-    }
-  }
-]);
+const easing = Easing.out(Easing.back(1));
 
 const App = () => {
-  const constrainedAnim = min(1, max(0, opacity ));
   return (
     <View style={{ flex: 1, alignItems: 'center', paddingTop: 100 }}>
-      <TapGestureHandler
-        onHandlerStateChange={handler}
-      >
-        <Animated.View
-          style={{
-            backgroundColor: color(
-              round(multiply(sub(1, constrainedAnim), 255)),
-              0,
-              0,
-              1,
-            ),
-            width: 200,
-            height: 60,
-            borderRadius: 20,
-            transform: [
-              { scale: add(1, multiply(opacity, 0.05)) },
-            ],
-            justifyContent: 'space-around',
-            alignItems: 'center',
-          }}
-        >
-          <Animated.Text
+      <Touchable easing={easing} duration={400}>
+        {({ anim, constrainedAnim }) => (
+          <Animated.View
             style={{
-              color: color(
-                255,
-                sub(255, round(multiply(constrainedAnim, 81))),
-                round(multiply(sub(1, constrainedAnim), 255)),
-                1,
+              backgroundColor: interpolateColor(
+                constrainedAnim,
+                '#FFFFFF',
+                '#000000',
               ),
-              fontSize: 26,
-              fontWeight: 'bold',
+              borderWidth: 1,
+              borderColor: 'black',
+              width: 200,
+              height: 60,
+              borderRadius: 5,
+              transform: [
+                { scale: sub(1, multiply(anim, 0.02)) },
+                { rotate: concat(multiply(anim, 180), 'deg') },
+              ],
+              justifyContent: 'space-around',
+              alignItems: 'center',
             }}
           >
-            HELLO THERE
-          </Animated.Text>
-        </Animated.View>
-      </TapGestureHandler>
+            <Animated.Text
+              style={{
+                color: interpolateColor(
+                  constrainedAnim,
+                  '#000000',
+                  '#FFFFFF',
+                ),
+                transform: [
+                  { rotate: concat(multiply(anim, -180), 'deg') },
+                  { perspective: 100 },
+                  { rotateX: concat(multiply(constrainedAnim, 360), 'deg') },
+                ],
+                fontSize: 26,
+                fontWeight: 'bold',
+              }}
+            >
+              HELLO THERE
+            </Animated.Text>
+          </Animated.View>
+        )}
+      </Touchable>
     </View>
   );
 }
