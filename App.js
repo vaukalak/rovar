680c223de684d35084477acbdb73c377d4ab2bba
vaukalak <lahmataja.pa4vara@gmail.com>
2019-05-01 13:35:43 +0300

    more style enhancers

--- App.js
+++ App.js
@@ -1,10 +1,18 @@
 import React, { useState } from 'react';
 import { View, StyleSheet } from 'react-native';
 import Animated, { Easing } from 'react-native-reanimated';
-import FlipAndTintTouchable from './FlipAndTintTouchable';
-import CirclePressTouchable from './CirclePressTouchable';
-import ScaleTouchable from './ScaleTouchable';
-import TouchableOpacity from './TouchableOpacity';
+import createTouchableComponent from './lib/createTouchableComponent';
+import circleAnimationEnhancer from './lib/enhancers/circleAnimationEnhancer';
+import { backgroundColorStyleCreator } from './lib/enhancers/styles/backgroundColorEnhancer';
+import { colorStyleCreator } from './lib/enhancers/styles/colorStyleEnhancer';
+import { opacityStyleCreator } from './lib/enhancers/styles/opacityEnhancer';
+import { rotateStyleCreator } from './lib/enhancers/styles/rotateEnhancer';
+import firstChildEnhancer from './lib/enhancers/firstChildEnhancer';
+import scaleEnhancer, { scaleStyleCreator } from './lib/enhancers/styles/scaleEnhancer';
+import compose from './lib/compose';
+import composeStyleCreators from './lib/composeStyleCreators';
+import styleEnhancer from './lib/enhancers/styleEnhancer';
+import {translateStyleCreator} from './lib/enhancers/styles/translateEnhancer';
 
 const easing = Easing.out(Easing.back(1));
 
@@ -29,6 +37,54 @@ const styles = StyleSheet.create({
   },
 });
 
+const CircleAnimationTouchable = createTouchableComponent(
+  compose(
+    circleAnimationEnhancer('#8f0013'),
+    firstChildEnhancer(
+      styleEnhancer(
+        composeStyleCreators(
+          scaleStyleCreator,
+          composeStyleCreators(
+            translateStyleCreator,
+            colorStyleCreator('#000000', '#FFFFFF'),
+          ),
+        ),
+      ),
+    ),
+  ),
+);
+
+const ScaleOpacityTouchable = createTouchableComponent(
+  styleEnhancer(
+    composeStyleCreators(
+      opacityStyleCreator,
+      scaleStyleCreator,
+    ),
+  ),
+);
+
+const FlipAndTintTouchable = createTouchableComponent(
+  compose(
+    firstChildEnhancer(
+      styleEnhancer(
+        composeStyleCreators(
+          colorStyleCreator('#000000', '#FFFFFF'),
+          rotateStyleCreator,
+        )
+      )
+    ),
+    styleEnhancer(
+      composeStyleCreators(
+        scaleStyleCreator,
+        composeStyleCreators(
+          backgroundColorStyleCreator,
+          rotateStyleCreator,
+        ),
+      ),
+    ),
+  ),
+);
+
 const App = () => {
   const [handlers] = useState(() => ({
     onPressIn: () => { console.log('onPressIn'); },
@@ -37,34 +93,34 @@ const App = () => {
   }));
   return (
     <View style={styles.container}>
-      {/*<TouchableOpacity*/}
-        {/*easing={easing}*/}
-        {/*duration={200}*/}
-        {/*{...handlers}*/}
-      {/*>*/}
-        {/*<Animated.View style={styles.button}>*/}
-          {/*<Animated.Text style={styles.label}>*/}
-            {/*TOUCHABLE OPACITY*/}
-          {/*</Animated.Text>*/}
-        {/*</Animated.View>*/}
-      {/*</TouchableOpacity>*/}
-      {/*<FlipAndTintTouchable*/}
-        {/*easing={easing}*/}
-        {/*duration={400}*/}
-        {/*backgroundColor="#FFFFFF"*/}
-        {/*backgroundTintColor="#000000"*/}
-        {/*textColor="#000000"*/}
-        {/*textTintColor="#FFFFFF"*/}
-        {/*{...handlers}*/}
-      {/*>*/}
-        {/*<Animated.View style={styles.button}>*/}
-          {/*<Animated.Text style={styles.label}>*/}
-            {/*GO*/}
-          {/*</Animated.Text>*/}
-        {/*</Animated.View>*/}
-      {/*</FlipAndTintTouchable>*/}
+      <ScaleOpacityTouchable
+        easing={easing}
+        duration={200}
+        {...handlers}
+      >
+        <Animated.View style={styles.button}>
+          <Animated.Text style={styles.label}>
+            TOUCHABLE OPACITY
+          </Animated.Text>
+        </Animated.View>
+      </ScaleOpacityTouchable>
+      <FlipAndTintTouchable
+        easing={easing}
+        duration={400}
+        backgroundColor="#FFFFFF"
+        backgroundTintColor="#000000"
+        textColor="#000000"
+        textTintColor="#FFFFFF"
+        {...handlers}
+      >
+        <Animated.View style={styles.button}>
+          <Animated.Text style={styles.label}>
+            GO
+          </Animated.Text>
+        </Animated.View>
+      </FlipAndTintTouchable>
       <View style={{ flexDirection: 'row' }}>
-        <CirclePressTouchable
+        <CircleAnimationTouchable
           easing={easing}
           duration={600}
           backgroundColor="rgba(240, 208, 0, 0.7)"
@@ -75,8 +131,8 @@ const App = () => {
               PRESS ONE
             </Animated.Text>
           </Animated.View>
-        </CirclePressTouchable>
-        <CirclePressTouchable
+        </CircleAnimationTouchable>
+        <CircleAnimationTouchable
           easing={easing}
           duration={600}
           backgroundColor="rgba(240, 208, 0, 0.7)"
@@ -87,7 +143,7 @@ const App = () => {
               PRESS TWO
             </Animated.Text>
           </Animated.View>
-        </CirclePressTouchable>
+        </CircleAnimationTouchable>
       </View>
     </View>
   );
--- FlipAndTintTouchable.js
+++ FlipAndTintTouchable.js
@@ -1,71 +1,52 @@
 import React from 'react';
 import Animated from 'react-native-reanimated';
 import interpolateColor from './lib/interpolateColor';
-import Touchable from './lib/Touchable';
+import createTouchableComponent from './lib/createTouchableComponent';
+import mergeStyles from './lib/mergeStyles';
 
-const { concat, multiply, sub } = Animated;
+const { concat, multiply } = Animated;
 
-const FlipAndTintTouchable = ({
-  children,
-  backgroundColor,
-  backgroundTintColor,
-  textColor,
-  textTintColor,
-  ...touchableProps,
-}) => {
-  React.Children.only(children);
-  return (
-    <Touchable {...touchableProps}>
-      {({ anim, constrainedAnim }) => {
-        const { style, children: subChildren, ...childProps } = children.props;
-        React.Children.only(subChildren);
+export const childrenEnhancer = (children, props, animationState) => {
+  const { children: subChildren } = children.props;
+  const {
+    backgroundColor,
+    backgroundTintColor,
+    textColor,
+    textTintColor,
+  } = props;
+  const {
+    constrainedAnim,
+    anim,
+  } = animationState;
+  React.Children.only(subChildren);
 
-        const { style: subChildStyle, ...subChildProps } = subChildren.props;
+  const { style: subChildStyle, ...subChildProps } = subChildren.props;
 
-        return React.cloneElement(
-          children,
+  return React.cloneElement(
+    children,
+    {},
+    React.cloneElement(
+      subChildren,
+      {
+        ...subChildProps,
+        style: [
           {
-            ...childProps,
-            style: [
-              {
-                backgroundColor: interpolateColor(
-                  constrainedAnim,
-                  backgroundColor,
-                  backgroundTintColor,
-                ),
-                transform: [
-                  { scale: sub(1, multiply(anim, 0.02)) },
-                  { rotate: concat(multiply(anim, 180), 'deg') },
-                ],
-              },
-              style,
+            color: interpolateColor(
+              constrainedAnim,
+              textColor,
+              textTintColor,
+            ),
+            transform: [
+              { rotate: concat(multiply(anim, -180), 'deg') },
+              { perspective: 100 },
+              { rotateX: concat(multiply(constrainedAnim, 360), 'deg') },
             ],
           },
-          React.cloneElement(
-            subChildren,
-            {
-              ...subChildProps,
-              style: [
-                {
-                  color: interpolateColor(
-                    constrainedAnim,
-                    textColor,
-                    textTintColor,
-                  ),
-                  transform: [
-                    { rotate: concat(multiply(anim, -180), 'deg') },
-                    { perspective: 100 },
-                    { rotateX: concat(multiply(constrainedAnim, 360), 'deg') },
-                  ],
-                },
-                subChildStyle,
-              ]
-            }
-          ),
-        )
-      }}
-    </Touchable>
-  );
+          subChildStyle,
+        ]
+      }
+    ),
+  )
 };
 
-export default FlipAndTintTouchable;
+export default createTouchableComponent(childrenEnhancer);
