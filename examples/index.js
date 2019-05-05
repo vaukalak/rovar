be6e3ec438ac04f2908774be74a5462c4bd033c2
vaukalak <lahmataja.pa4vara@gmail.com>
2019-05-05 19:52:57 +0300

    refactoring transitions step one

--- examples/index.js
+++ examples/index.js
@@ -1,4 +1,4 @@
-// import Example from './CreateTouchableExamples';
-import Example from './HooksExample';
+import Example from './CreateTouchableExamples';
+// import Example from './HooksExample';
 
 export default Example;
